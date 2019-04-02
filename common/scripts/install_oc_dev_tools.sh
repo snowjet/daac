5164ccfbc1227ecfaaa91a56afc52656704ee63b
Johan Swanepoel <johannes.swanepoel@gmail.com>
2019-04-02 16:27:16 +1100

    Adding OC client from Red Hat REPO

--- common/scripts/install_oc_dev_tools.sh
+++ common/scripts/install_oc_dev_tools.sh
@@ -1,7 +1,7 @@
 # Install OC client
 
 if [[ ! -z $OC_DEV_TOOLS  ]]; then
-    curl -L https://github.com/openshift/origin/releases/download/v3.11.0/openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit.tar.gz | tar xz
+    curl -L https://mirror.openshift.com/pub/openshift-v3/clients/3.11.100/linux/oc.tar.gz | tar xz
     mv openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit/oc /usr/local/bin
     mv openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit/kubectl /usr/local/bin
     rm -rf openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit
