234e036460483561ac39a5517eada31357971efd
Johan Swanepoel <johannes.swanepoel@gmail.com>
2019-04-02 14:19:03 +1100

    Install code -y

--- common/scripts/install_oc_dev_tools.sh
+++ common/scripts/install_oc_dev_tools.sh
@@ -13,7 +13,7 @@ if [[ ! -z $OC_DEV_TOOLS  ]]; then
     sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
 
     yum check-update
-    yum install code
+    yum install -y code
 
     yum autoremove -y
     yum clean all
