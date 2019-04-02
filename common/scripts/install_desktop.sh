a59dfa4411249741f1a8488e67136aebc68375e5
snowjet <rarm.jet@gmail.com>
2019-04-02 16:14:18 +1100

    fixed mate theming

--- common/scripts/install_desktop.sh
+++ common/scripts/install_desktop.sh
@@ -4,30 +4,21 @@
 case $DESKTOP in
      gnome3)
             echo "Installing Gnome 3 Destop"
-            yum install -y gnome-classic-session gnome-session gnome-terminal nautilus-open-terminal control-center liberation-mono-fonts
+            yum install -y gnome-classic-session gnome-session gnome-terminal nautilus-open-terminal control-center
             echo "PREFERRED=/usr/bin/gnome-session --session gnome-classic" > /etc/sysconfig/desktop
           ;;
      mate|*)
             echo "Installing Mate Destop"
             yum --setopt=group_package_types=mandatory groupinstall -y "Mate Desktop"
-            yum install -y adapta-gtk-theme liberation-mono-fonts mate-terminal
+            yum install -y numix-gtk-theme numix-icon-theme mate-terminal
             echo "PREFERRED=/usr/bin/mate-session" > /etc/sysconfig/desktop
 
-cat << EOF > /etc/dconf/db/local.d/00_mate_desktop
-[mate/desktop/interface]
-icon-theme='Numix'
-gtk-theme='Numix'
-EOF
-
-cat << EOF > /etc/dconf/db/local.d/locks/00_mate_desktop
-# Mate Themes
-/org/mate/desktop/interface/icon-theme
-/org/mate/desktop/interface/gtk-theme
-EOF
-
-dconf update
+            cp /tmp/config/dconf/00_mate_theme /etc/dconf/db/local.d/
+            dconf update
           ;;
 esac
+
+yum install liberation-fonts
 
 yum autoremove -y
 yum clean all
