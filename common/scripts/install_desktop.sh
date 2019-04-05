765e59ac6b8d764d8d7795fa8d7f9255f0faeec6
Johan Swanepoel <johannes.swanepoel@gmail.com>
2019-04-05 15:20:04 +1100

    adding information on how to run on OpenShift. Guac is working, xrdp not so muc

--- common/scripts/install_desktop.sh
+++ common/scripts/install_desktop.sh
@@ -7,7 +7,7 @@ case $DESKTOP in
             yum install -y gnome-classic-session gnome-session gnome-terminal nautilus-open-terminal control-center
             echo "PREFERRED=/usr/bin/gnome-session --session gnome-classic" > /etc/sysconfig/desktop
           ;;
-     mate|*)
+     mate)
             echo "Installing Mate Destop"
             yum --setopt=group_package_types=mandatory groupinstall -y "Mate Desktop"
             yum install -y numix-gtk-theme numix-icon-theme mate-terminal
@@ -16,7 +16,20 @@ case $DESKTOP in
             cp /tmp/config/dconf/00_mate_theme /etc/dconf/db/local.d/
             dconf update
           ;;
+     xfce|*)
+            echo "Installing XFCE Destop"
+            yum -y groupinstall "X Window system"
+            yum -y groupinstall --setopt=group_package_types=mandatory xfce
+            echo "PREFERRED=/usr/bin/startxfce4" > /etc/sysconfig/desktop
+
+          ;;
 esac
+
+touch /run/dbus/messagebus.pid
+touch /run/dbus/system_bus_socket.pid
+chgrp -R 0 /run/dbus/messagebus.pid && chmod -R g=u /run/dbus/messagebus.pid
+chgrp -R 0 /run/dbus/system_bus_socket.pid && chmod -R g=u /run/dbus/system_bus_socket.pid
+chmod 777 /run/dbus/system_bus_socket
 
 yum install -y liberation-fonts
 
--- common/scripts/install_xrdp.sh
+++ common/scripts/install_xrdp.sh
@@ -4,6 +4,17 @@ yum install -y xrdp xorgxrdp
 
 cp /tmp/config/xrdp/xrdp.ini /etc/xrdp/xrdp.ini
 cp /tmp/config/xrdp/sesman.ini /etc/xrdp/sesman.ini
+touch /var/log/xrdp-sesman.log
+chgrp -R 0 /var/log/xrdp-sesman.log
+chmod -R g=u /var/log/xrdp-sesman.log
+
+touch /var/log/xrdp.log
+chgrp -R 0 /var/log/xrdp-sesman.log
+chmod -R g=u /var/log/xrdp-sesman.log
+
+
+chgrp -R 0 /etc/xrdp
+chmod -R g=u /etc/xrdp
 
 if [[ ! -z $SYSTEMD  ]]; then
         systemctl enable tomcat
