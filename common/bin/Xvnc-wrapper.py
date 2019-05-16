3a622ef36cb35c0a0d0641748822cf9b4996e5fa
snowjet <rarm.jet@gmail.com>
2019-05-16 15:57:00 +1000

    arg parser now uses positional detection to determine display output

--- common/bin/Xvnc-wrapper.py
+++ common/bin/Xvnc-wrapper.py
@@ -16,14 +16,24 @@ parser.add_argument("-auth")
 parser.add_argument("-geometry")
 parser.add_argument("-depth")
 parser.add_argument("-dpi")
-parser.add_argument(":")
+parser.add_argument("-nolisten")
+parser.add_argument("-bs", action='store_true', default=False)
+parser.add_argument("-localhost", action='store_true', default=False)
+parser.add_argument("-rfbauth", action='store_true', default=False)
+parser.add_argument("display")
 args, unknown_args = parser.parse_known_args()
 
 display_env = os.environ.copy()
-display_env["DISPLAY"] = ":10"
+display_env["DISPLAY"] = args.display
 display_env["HOME"] = "/home/user"
 
 logging.debug('Commands Received:' + str(args) + str(unknown_args))
+
+# single session only
+logging.debug("reap any hangged sessions")
+xvnc = subprocess.Popen("vncserver -kill :10", shell=True, cwd="/home/user",
+                        env=display_env, stderr=subprocess.PIPE, 
+                        stdout=subprocess.PIPE)
 
 xvnc_cmd = "/usr/bin/vncserver :10 -name user -geometry %s \
             -depth %s -noxstartup -auth .Xauthority -bs \
